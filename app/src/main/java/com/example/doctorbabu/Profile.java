4f08acc8694de3b58a1fae7c385c6930222694b1
Nafis71 <nhtonmoy2@gmail.com>
2023-05-22 04:38:21 +0600

    added new UI

--- app/src/main/java/com/example/doctorbabu/Profile.java
+++ app/src/main/java/com/example/doctorbabu/Profile.java
@@ -7,15 +7,21 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import android.util.Log;
+import android.util.TypedValue;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.ProgressBar;
+import android.widget.RelativeLayout;
+import android.widget.TableLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 import com.bumptech.glide.Glide;
+import com.google.android.flexbox.FlexDirection;
+import com.google.android.flexbox.FlexboxLayout;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
@@ -23,17 +29,23 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
 
 public class Profile extends Fragment {
 
     FirebaseAuth auth;
     FirebaseUser user;
-    TextView email,phone,verificationStatus,fullName,address,age;
+    TextView email,phone,verificationStatus,fullName,alergy;
     ImageView userprofilePicture,verifyTickSign,notVerifyImg;
-    Button editProfile;
+    Button editProfile,addUserPersonalHistory;
     ProgressBar loadingCircle;
+    FlexboxLayout flex;
+    ArrayList<String> alergies = new ArrayList<String>();
     public Profile() {
         // Required empty public constructor
     }
@@ -74,6 +86,9 @@ public class Profile extends Fragment {
         notVerifyImg = (ImageView) getView().findViewById(R.id.notVerified);
         editProfile = (Button) getView().findViewById(R.id.editProfile);
         fullName = (TextView) getView().findViewById(R.id.userName);
+        flex = (FlexboxLayout) getView().findViewById(R.id.alergyHistory);
+        alergy =(TextView) getView().findViewById(R.id.alergy);
+        userAlergyHistory();
 //        phone = (TextView) getView().findViewById(R.id.userPhone);
 //        address = (TextView) getView().findViewById(R.id.userAddress);
 //        age = (TextView) getView().findViewById(R.id.userAge);
@@ -143,6 +158,103 @@ public class Profile extends Fragment {
             }
         });
     }
+  public void userAlergyHistory(){
+        FirebaseDatabase database = FirebaseDatabase.getInstance("https://prescription-bf7c7-default-rtdb.asia-southeast1.firebasedatabase.app");
+        DatabaseReference reference = database.getReference("alergy");
+        reference.child(user.getUid()).get().addOnCompleteListener(new OnCompleteListener<DataSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DataSnapshot> task) {
+                if(task.isSuccessful())
+                {
+                    if(task.getResult().exists())
+                    {
+                        DataSnapshot snapshot = task.getResult();
+                        String cloth = String.valueOf(snapshot.child("Cloth").getValue());
+                        String drug = String.valueOf(snapshot.child("Drug").getValue());
+                        ArrayList<String> list = new ArrayList<>();
+                        if(!cloth.equals("null"))
+                        {
+                            list.add(cloth);
+                        }
+                        if(!drug.equals("null"))
+                        {
+                            list.add(drug);
+                        }
+                        alergyListAdd(list);
+                    }
+                }
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+
+            }
+        });
+  }
+  public void alergyListAdd(ArrayList <String> list)
+  {
+      int size = list.size();
+      if(size != 0)
+      {
+          alergy.setVisibility(View.GONE);
+          for(int i=0;i<size;i++)
+          {
+              TextView text = new TextView(getContext());FlexboxLayout.LayoutParams layout =
+                  new FlexboxLayout.LayoutParams(
+                          FlexboxLayout.LayoutParams.WRAP_CONTENT,
+                          FlexboxLayout.LayoutParams.WRAP_CONTENT);
+
+              layout.setLayoutDirection(FlexDirection.ROW);
+              layout.setMarginStart(10);
+              text.setTextSize(TypedValue.COMPLEX_UNIT_SP,16);
+              text.setLayoutParams(layout);
+              if(i == size-1)
+              {
+                  text.setText(list.get(i));
+              }
+              else
+              {
+                  String value = list.get(i) + ",";
+                  text.setText(value);
+              }
+              flex.addView(text);
+          }
+      }
+      else
+      {
+          alergy.setVisibility(View.VISIBLE);
+      }
+
+//      TextView text = new TextView(getContext());
+//      TextView text2 = new TextView(getContext());
+//    FlexboxLayout.LayoutParams layout =
+//              new FlexboxLayout.LayoutParams(
+//                      FlexboxLayout.LayoutParams.WRAP_CONTENT,
+//                      FlexboxLayout.LayoutParams.WRAP_CONTENT);
+//
+//      layout.setLayoutDirection(FlexDirection.ROW);
+//      layout.setMarginStart(10);
+//      text.setLayoutParams(layout);
+//      text2.setLayoutParams(layout);
+//      text.setText(cloth);
+//      text2.setText(drug);
+//      flex.addView(text);
+//      flex.addView(text2);
+
+//// Right subregions contains a Textview
+//      TextView tv = new TextView(getContext());
+//      TextView tv2 = new TextView(getContext());
+//      tv.setLayoutParams(lpRight);
+//      tv2.setLayoutParams(lpRight);
+//      tv.setText("TV");
+//      tv.remove
+//      tv2.setText("TV2");
+//      flex.addView(tv);
+//      flex.addView(tv2);
+//      flex.remo
+
+
+  }
     public void alertDialog(String title,String message,String button,String button2)
     {
         AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
