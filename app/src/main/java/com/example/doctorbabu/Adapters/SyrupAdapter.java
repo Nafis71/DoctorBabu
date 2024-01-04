e6a3af620103005d8321915922652b22e959cf9a
Nafis71 <nhtonmoy2@gmail.com>
2024-01-04 13:47:34 +0600

    Syrup details module 80% completed

--- app/src/main/java/com/example/doctorbabu/Adapters/SyrupAdapter.java
+++ app/src/main/java/com/example/doctorbabu/Adapters/SyrupAdapter.java
@@ -1,6 +1,7 @@
 package com.example.doctorbabu.Adapters;
 
 import android.content.Context;
+import android.content.Intent;
 import android.text.Layout;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -10,17 +11,35 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.bumptech.glide.Glide;
+import com.example.doctorbabu.DatabaseModels.MedicineModel;
 import com.example.doctorbabu.DatabaseModels.syrupModel;
+import com.example.doctorbabu.FirebaseDatabase.Firebase;
 import com.example.doctorbabu.R;
+import com.example.doctorbabu.patient.HomeModules.MedicineDetails;
+import com.example.doctorbabu.patient.HomeModules.SyrupDetails;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.card.MaterialCardView;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class SyrupAdapter extends RecyclerView.Adapter<SyrupAdapter.myViewHolder> {
     Context context;
     ArrayList<syrupModel> model;
+    ExecutorService tracker;
 
     public SyrupAdapter(Context context, ArrayList<syrupModel> model) {
         this.context = context;
@@ -41,10 +60,73 @@ public class SyrupAdapter extends RecyclerView.Adapter<SyrupAdapter.myViewHolder
         holder.syrupName.setText(dbModel.getSyrupName());
         holder.syrupBottleSize.setText(dbModel.getBottleSize());
         holder.syrupPrice.setText(dbModel.getUnitPrice());
+        tracker = Executors.newSingleThreadExecutor();
+        holder.card.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                tracker.execute(new Runnable() {
+                    @Override
+                    public void run() {
+                        trackMedicine(dbModel);
+                    }
+                });
+            }
+        });
     }
+    public void trackMedicine(syrupModel dbModel) {
+        Firebase firebase = Firebase.getInstance();
+        FirebaseUser user = firebase.getUserID();
+        DatabaseReference trackReference = firebase.getDatabaseReference("trackMedicine");
+        trackReference.child(dbModel.getSyrupId()).child(user.getUid()).addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                if (!snapshot.exists()) {
+                    saveRecord(dbModel, trackReference, user);
+                } else {
+                    launchMedicineDetails(dbModel);
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                throw error.toException();
+            }
+        });
+    }
+
+    public void saveRecord(syrupModel dbModel, DatabaseReference trackReference, FirebaseUser user) {
+        HashMap<String, String> data = new HashMap<>();
+        data.put("count", String.valueOf(1));
+        trackReference.child(dbModel.getSyrupId()).child(user.getUid()).setValue(data).addOnCompleteListener(new OnCompleteListener<Void>() {
+            @Override
+            public void onComplete(@NonNull Task<Void> task) {
+                launchMedicineDetails(dbModel);
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                //do nothing
+            }
+        });
+    }
+
+    public void launchMedicineDetails(syrupModel dbModel) {
+        AppCompatActivity activity = (AppCompatActivity) context;
+        Intent intent = new Intent(context, SyrupDetails.class);
+        intent.putExtra("medicineId", dbModel.getSyrupId());
+        if (context instanceof MedicineDetails) {
+            activity.startActivity(intent);
+            activity.finish();
+            tracker.shutdown();
+        } else {
+            activity.startActivity(intent);
+        }
+    }
+
     public static class myViewHolder extends RecyclerView.ViewHolder{
         ImageView syrupImage;
         TextView syrupName,syrupPrice,syrupBottleSize;
+        MaterialCardView card;
 
         public myViewHolder(@NonNull View itemView) {
             super(itemView);
@@ -52,6 +134,7 @@ public class SyrupAdapter extends RecyclerView.Adapter<SyrupAdapter.myViewHolder
             syrupName =  itemView.findViewById(R.id.syrupName);
             syrupBottleSize = itemView.findViewById(R.id.syrupBottleSize);
             syrupPrice = itemView.findViewById(R.id.syrupPrice);
+            card = itemView.findViewById(R.id.card);
         }
     }
 
--- app/src/main/java/com/example/doctorbabu/patient/HomeModules/SyrupDetails.java
+++ app/src/main/java/com/example/doctorbabu/patient/HomeModules/SyrupDetails.java
@@ -1,26 +1,151 @@
 package com.example.doctorbabu.patient.HomeModules;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.app.Dialog;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 
+import com.bumptech.glide.Glide;
+import com.example.doctorbabu.FirebaseDatabase.Firebase;
 import com.example.doctorbabu.R;
 import com.example.doctorbabu.databinding.ActivitySyrupDetailsBinding;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.Formatter;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class SyrupDetails extends AppCompatActivity {
     ActivitySyrupDetailsBinding binding;
+    Dialog dialog;
+    ArrayList<String> bottleList;
+    ExecutorService syrupDataExecutor;
+    Firebase firebase;
+    FirebaseUser user;
+    int bottle = 1,unitPrice,syrupQuantity;
+    double syrupPrice;
+    String medicineId,syrupGenericName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = ActivitySyrupDetailsBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+        loadingScreen();
+        medicineId = getIntent().getStringExtra("medicineId");
+        firebase = Firebase.getInstance();
+        user = firebase.getUserID();
+        syrupDataExecutor = Executors.newSingleThreadExecutor();
+        syrupDataExecutor.execute(new Runnable() {
+            @Override
+            public void run() {
+                loadSyrupData();
+            }
+        });
+        closeLoadingScreen();
     }
 
+    public void loadingScreen() {
+        dialog = new Dialog(this);
+        dialog.setContentView(R.layout.loading_screen);
+        dialog.setCancelable(false);
+        dialog.show();
+    }
+    public void closeLoadingScreen() {
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                generateBottles();
+                binding.syrupImage.setVisibility(View.VISIBLE);
+                binding.purchaseLayout.setVisibility(View.VISIBLE);
+                binding.relativeBrandLayout.setVisibility(View.VISIBLE);
+                binding.syrupDescriptionLayout.setVisibility(View.VISIBLE);
+                binding.medicalOverViewLayout.setVisibility(View.VISIBLE);
+                dialog.dismiss();
+            }
+        }, 2000);
+    }
+
+    public void generateBottles() {
+        bottleList = new ArrayList<>();
+        for (int i = 0; i < 200; i++) {
+            if (i == 0) {
+                String sheetString = (i + 1) + " " + "Bottle";
+                bottleList.add(sheetString);
+            } else {
+                String sheetString = (i + 1) + " " + "Bottles";
+                bottleList.add(sheetString);
+            }
+        }
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, R.layout.drop_menu, bottleList);
+        binding.bottle.setAdapter(adapter);
+        setBottleListener();
+    }
+    public void setBottleListener() {
+        binding.bottle.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                String sheetText = binding.bottle.getText().toString();
+                String[] sheetTextArray = sheetText.split(" ");
+                bottle = Integer.parseInt(sheetTextArray[0]);
+                syrupPrice = (bottle * unitPrice);
+                binding.medicinePrice.setText(String.valueOf(syrupPrice));
+            }
+        });
+    }
+
+    public void loadSyrupData() {
+        DatabaseReference reference = firebase.getDatabaseReference("syrupData");
+        reference.child(medicineId).addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                if (snapshot.exists()) {
+                    setViews(snapshot);
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                throw error.toException();
+            }
+        });
+    }
+    public void setViews(@NonNull DataSnapshot snapshot) {
+        Glide.with(this).load(String.valueOf(snapshot.child("syrupPicture").getValue())).into(binding.syrupImage);
+        binding.syrupName.setText(String.valueOf(snapshot.child("syrupName").getValue()));
+        binding.description.setText(String.valueOf(snapshot.child("description").getValue()));
+        binding.syrupBottleSize.setText(String.valueOf(snapshot.child("bottleSize").getValue()));
+        syrupGenericName = String.valueOf(snapshot.child("genericName").getValue());
+        binding.syrupGeneric.setText(syrupGenericName);
+        binding.syrupBrandName.setText(String.valueOf(snapshot.child("brandName").getValue()));
+        binding.syrupAdministration.setText(String.valueOf(snapshot.child("administrationOfTheMedicine").getValue()));
+        binding.sideEffect.setText(String.valueOf(snapshot.child("sideEffect").getValue()));
+        binding.storageCondition.setText(String.valueOf(snapshot.child("storageCondition").getValue()));
+        syrupQuantity = Integer.parseInt(String.valueOf(snapshot.child("syrupQuantity").getValue()));
+        calculatePrice(snapshot);
+    }
+    public void calculatePrice(@NonNull DataSnapshot snapshot) {
+        unitPrice = Integer.parseInt(String.valueOf(snapshot.child("unitPrice").getValue()));
+        syrupPrice = (bottle * unitPrice);
+        binding.medicinePrice.setText(String.valueOf(syrupPrice));
+    }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
         binding = null;
+        syrupDataExecutor.shutdown();
     }
 }
