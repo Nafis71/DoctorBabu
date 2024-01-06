72e2fd40768fb8cbf37bab3bdc421f11d677e084
Nafis71 <nhtonmoy2@gmail.com>
2024-01-06 20:13:44 +0600

    Bug Fixes

--- app/src/main/java/com/example/doctorbabu/Adapters/CheckoutAdapter.java
+++ app/src/main/java/com/example/doctorbabu/Adapters/CheckoutAdapter.java
@@ -10,14 +10,23 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.bumptech.glide.Glide;
 import com.example.doctorbabu.DatabaseModels.CartModel;
+import com.example.doctorbabu.FirebaseDatabase.Firebase;
 import com.example.doctorbabu.R;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class CheckoutAdapter extends RecyclerView.Adapter<CheckoutAdapter.myViewHolder> {
     Context context;
     ArrayList<CartModel> model;
+    ExecutorService medicineDataExecutor;
 
     public CheckoutAdapter(Context context, ArrayList<CartModel> model) {
         this.context = context;
@@ -27,19 +36,91 @@ public class CheckoutAdapter extends RecyclerView.Adapter<CheckoutAdapter.myView
     @NonNull
     @Override
     public CheckoutAdapter.myViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View view = LayoutInflater.from(context).inflate(R.layout.single_row_design_checkout_medicine_layout,parent,false);
+        View view = LayoutInflater.from(context).inflate(R.layout.single_row_design_checkout_medicine_layout, parent, false);
         return new myViewHolder(view);
     }
 
     @Override
     public void onBindViewHolder(@NonNull CheckoutAdapter.myViewHolder holder, int position) {
+        medicineDataExecutor = Executors.newSingleThreadExecutor();
         CartModel dbModel = model.get(position);
         holder.medicineQuantity.setText(dbModel.getQuantity());
+        holder.medicinePrice.setText(dbModel.getTotalPrice());
+        medicineDataExecutor.execute(new Runnable() {
+            @Override
+            public void run() {
+                fetchMedicineData(holder, dbModel);
+            }
+        });
 
     }
-    public static class myViewHolder extends RecyclerView.ViewHolder{
+
+    public void fetchMedicineData(CheckoutAdapter.myViewHolder holder, CartModel dbModel) {
+        Firebase firebase = Firebase.getInstance();
+        if (dbModel.getMedicineType().equalsIgnoreCase("tablet")) {
+            DatabaseReference reference = firebase.getDatabaseReference("medicineData");
+            reference.child(dbModel.getMedicineId()).addListenerForSingleValueEvent(new ValueEventListener() {
+                @Override
+                public void onDataChange(@NonNull DataSnapshot snapshot) {
+                    if (snapshot.exists()) {
+                        Glide.with(context).load(String.valueOf(snapshot.child("medicinePicture").getValue())).into(holder.medicineImage);
+                        holder.medicineName.setText(String.valueOf(snapshot.child("medicineName").getValue()));
+                        holder.medicineDosageOrbottleSize.setText(String.valueOf(snapshot.child("medicineDosage").getValue()));
+                    }
+                }
+
+                @Override
+                public void onCancelled(@NonNull DatabaseError error) {
+                    throw error.toException();
+                }
+            });
+        } else if (dbModel.getMedicineType().equalsIgnoreCase("syrup")) {
+            DatabaseReference reference = firebase.getDatabaseReference("syrupData");
+            reference.child(dbModel.getMedicineId()).addListenerForSingleValueEvent(new ValueEventListener() {
+                @Override
+                public void onDataChange(@NonNull DataSnapshot snapshot) {
+                    if (snapshot.exists()) {
+                        Glide.with(context).load(String.valueOf(snapshot.child("syrupPicture").getValue())).into(holder.medicineImage);
+                        holder.medicineName.setText(String.valueOf(snapshot.child("syrupName").getValue()));
+                        holder.medicineDosageOrbottleSize.setText(String.valueOf(snapshot.child("bottleSize").getValue()));
+                    }
+                }
+
+                @Override
+                public void onCancelled(@NonNull DatabaseError error) {
+                    throw error.toException();
+                }
+            });
+        } else if (dbModel.getMedicineType().equalsIgnoreCase("herbalSyrup")) {
+            DatabaseReference reference = firebase.getDatabaseReference("herbalSyrupData");
+            reference.child(dbModel.getMedicineId()).addListenerForSingleValueEvent(new ValueEventListener() {
+                @Override
+                public void onDataChange(@NonNull DataSnapshot snapshot) {
+                    if (snapshot.exists()) {
+                        Glide.with(context).load(String.valueOf(snapshot.child("syrupPicture").getValue())).into(holder.medicineImage);
+                        holder.medicineName.setText(String.valueOf(snapshot.child("syrupName").getValue()));
+                        holder.medicineDosageOrbottleSize.setText(String.valueOf(snapshot.child("bottleSize").getValue()));
+                    }
+                }
+
+                @Override
+                public void onCancelled(@NonNull DatabaseError error) {
+                    throw error.toException();
+                }
+            });
+        }
+        medicineDataExecutor.shutdown();
+    }
+
+    @Override
+    public int getItemCount() {
+        return model.size();
+    }
+
+    public static class myViewHolder extends RecyclerView.ViewHolder {
         ImageView medicineImage;
-        TextView medicineName,medicineDosageOrbottleSize,medicinePrice,medicineQuantity;
+        TextView medicineName, medicineDosageOrbottleSize, medicinePrice, medicineQuantity;
+
         public myViewHolder(@NonNull View itemView) {
             super(itemView);
             medicineImage = itemView.findViewById(R.id.medicineImage);
@@ -48,10 +129,5 @@ public class CheckoutAdapter extends RecyclerView.Adapter<CheckoutAdapter.myView
             medicinePrice = itemView.findViewById(R.id.medicinePrice);
             medicineQuantity = itemView.findViewById(R.id.medicineQuantity);
         }
-    }
-
-    @Override
-    public int getItemCount() {
-        return model.size();
     }
 }
--- app/src/main/java/com/example/doctorbabu/patient/HomeModules/Checkout.java
+++ app/src/main/java/com/example/doctorbabu/patient/HomeModules/Checkout.java
@@ -1,15 +1,13 @@
 package com.example.doctorbabu.patient.HomeModules;
 
+import android.app.Dialog;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
-
-import android.app.Dialog;
-import android.content.Intent;
-import android.os.Bundle;
-import android.os.Handler;
-import android.util.Log;
-import android.view.View;
 
 import com.example.doctorbabu.Adapters.CheckoutAdapter;
 import com.example.doctorbabu.DatabaseModels.CartModel;
@@ -31,7 +29,7 @@ public class Checkout extends AppCompatActivity {
     ArrayList<String> selectedCards;
     Dialog dialog;
     String totalPrice;
-    ExecutorService customerDataExecutor,checkoutListExecutor;
+    ExecutorService customerDataExecutor, checkoutListExecutor, loadSelectedCardExecutor, rewardExecutor;
     Firebase firebase;
     FirebaseUser user;
     CheckoutAdapter adapter;
@@ -47,6 +45,14 @@ public class Checkout extends AppCompatActivity {
         initializeFirebase();
         customerDataExecutor = Executors.newSingleThreadExecutor();
         checkoutListExecutor = Executors.newSingleThreadExecutor();
+        loadSelectedCardExecutor = Executors.newSingleThreadExecutor();
+        rewardExecutor = Executors.newSingleThreadExecutor();
+        loadSelectedCardExecutor.execute(new Runnable() {
+            @Override
+            public void run() {
+                loadSelectedCards();
+            }
+        });
         customerDataExecutor.execute(new Runnable() {
             @Override
             public void run() {
@@ -60,19 +66,39 @@ public class Checkout extends AppCompatActivity {
             }
         });
         closeLoadingScreen();
+        binding.back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+        rewardExecutor.execute(new Runnable() {
+            @Override
+            public void run() {
+                binding.applyReward.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        addReward();
+                    }
+                });
+            }
+        });
+
     }
 
-    public void initializeFirebase(){
+    public void initializeFirebase() {
         firebase = Firebase.getInstance();
         user = firebase.getUserID();
     }
 
-    public void loadSelectedCards(){
+    public void loadSelectedCards() {
         selectedCards = getIntent().getStringArrayListExtra("selectedCards");
         totalPrice = getIntent().getStringExtra("totalPrice");
         assert totalPrice != null;
-        binding.totalPrice.setText(String.valueOf(Double.parseDouble(totalPrice) + 60));
-        String totalCheckout = "("+selectedCards.size()+")";
+        binding.totalPrice.setText(String.valueOf(Math.round(Double.parseDouble(totalPrice) + 60)));
+        binding.itemsTotalPrice.setText(String.valueOf(Math.round(Double.parseDouble(totalPrice))));
+        binding.totalPayment.setText(String.valueOf(Math.round(Double.parseDouble(totalPrice) + 60)));
+        String totalCheckout = "(" + selectedCards.size() + ")";
         binding.totalCheckOut.setText(totalCheckout);
     }
 
@@ -88,51 +114,57 @@ public class Checkout extends AppCompatActivity {
         handler.postDelayed(new Runnable() {
             @Override
             public void run() {
+                binding.bodyLayout.setVisibility(View.VISIBLE);
+                binding.placeOrderLayout.setVisibility(View.VISIBLE);
                 dialog.dismiss();
             }
         }, 2000);
     }
 
-    public void loadCustomerData(){
+    public void loadCustomerData() {
         DatabaseReference customerReference = firebase.getDatabaseReference("users");
         customerReference.child(user.getUid()).addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot snapshot) {
-                if(snapshot.exists()){
+                if (snapshot.exists()) {
                     setDeliveryDetailsViews(snapshot);
                 }
             }
+
             @Override
             public void onCancelled(@NonNull DatabaseError error) {
                 throw error.toException();
             }
         });
     }
-    public void setDeliveryDetailsViews(DataSnapshot snapshot){
+
+    public void setDeliveryDetailsViews(DataSnapshot snapshot) {
         binding.customerName.setText(String.valueOf(snapshot.child("fullName").getValue()));
         binding.deliveryAddress.setText(String.valueOf(snapshot.child("address").getValue()));
         binding.phoneNumber.setText(String.valueOf(snapshot.child("phone").getValue()));
     }
-    public void initializeCheckoutRecyclerView(){
+
+    public void initializeCheckoutRecyclerView() {
         checkoutModels = new ArrayList<>();
-        binding.checkoutMedicineRecyclerView.setLayoutManager(new LinearLayoutManager(Checkout.this,LinearLayoutManager.VERTICAL,false),R.layout.shimmer_layout_medicine);
-        adapter = new CheckoutAdapter(this,checkoutModels);
+        binding.checkoutMedicineRecyclerView.setLayoutManager(new LinearLayoutManager(Checkout.this, LinearLayoutManager.VERTICAL, false), R.layout.shimmer_layout_medicine);
+        adapter = new CheckoutAdapter(this, checkoutModels);
         binding.checkoutMedicineRecyclerView.showShimmer();
         checkCart();
     }
 
-    public void checkCart(){
+    public void checkCart() {
         DatabaseReference cartReference = firebase.getDatabaseReference("medicineCart");
-        for(String medicineId : selectedCards){
+        for (String medicineId : selectedCards) {
             cartReference.child(user.getUid()).child(medicineId).addListenerForSingleValueEvent(new ValueEventListener() {
                 @Override
                 public void onDataChange(@NonNull DataSnapshot snapshot) {
-                    if(snapshot.exists()){
+                    if (snapshot.exists()) {
                         CartModel cartModel = snapshot.getValue(CartModel.class);
                         checkoutModels.add(cartModel);
                         binding.checkoutMedicineRecyclerView.setAdapter(adapter);
                     }
                 }
+
                 @Override
                 public void onCancelled(@NonNull DatabaseError error) {
                     throw error.toException();
@@ -142,10 +174,26 @@ public class Checkout extends AppCompatActivity {
         binding.checkoutMedicineRecyclerView.hideShimmer();
     }
 
+    public void addReward() {
+        String rewardPoint = binding.rewardPoint.getText().toString().trim();
+        if(!rewardPoint.isEmpty()){
+            if (Integer.parseInt(rewardPoint) >= 100) {
+                checkReward(Integer.parseInt(rewardPoint));
+            } else {
+                binding.rewardPointTextLayout.setError("Minimum reward points that can be used is 100");
+            }
+        }
+    }
+
+    public void checkReward(int rewardPoint) {
+
+    }
+
     @Override
     protected void onDestroy() {
         super.onDestroy();
         binding = null;
         customerDataExecutor.shutdown();
+        loadSelectedCardExecutor.shutdown();
     }
 }
