5abe26af734afe3c042c2ea7f943215989245e69
Nafis71 <nhtonmoy2@gmail.com>
2024-01-08 22:20:14 +0600

    Some bug fixes

--- app/src/main/java/com/example/doctorbabu/patient/HomeModules/Checkout.java
+++ app/src/main/java/com/example/doctorbabu/patient/HomeModules/Checkout.java
@@ -80,7 +80,7 @@ public class Checkout extends AppCompatActivity {
         binding.back.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                finish();
+                onBackPressed();
             }
         });
         rewardExecutor.execute(new Runnable() {
